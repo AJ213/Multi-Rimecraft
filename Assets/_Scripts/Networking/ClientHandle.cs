37917a9aa48461a999c954c85bbcea6b06ba3f4e
Ayrton <ayrtonmuniz2002@gmail.com>
2022-02-12 22:13:13 -0800

    Run Server and Building Server

--- Assets/_Scripts/Networking/ClientHandle.cs
+++ Assets/_Scripts/Networking/ClientHandle.cs
@@ -31,10 +31,6 @@ public class ClientHandle : MonoBehaviour
     {
         int id = packet.ReadInt();
         Vector3 position = packet.ReadVector3();
-        /*if (Client.instance.myId != id)
-        {
-            Debug.Log("read the position: " + position);
-        }*/
 
         GameManager.players[id].transform.position = position;
     }
